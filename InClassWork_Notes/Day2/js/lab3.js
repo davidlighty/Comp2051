7a8560b1bf949ea0a5d5f424d482a2b476926035
David Lighty <david.lighty@gmail.com>
2013-09-18 23:32:26 -0700

    Added more documentation to lab3.js
    Wanted to add more comments to the code for others that might look.

--- InClassWork_Notes/Day2/js/lab3.js
+++ InClassWork_Notes/Day2/js/lab3.js
@@ -17,15 +17,18 @@
 */
 
 // Global
-var arrModule;
-var msg;
+var arrModule; // util.js
+var msg; // util.js
 
+// Main Module - this holds the init and page creation logic  ((Controller))
 var labModule = function() {
-	var htmlModule;
+	var htmlModule; // This doesn't need to be a global.
 
-	var d = document;
+	var d = document; // shortcut variable, I just don't want to write document everywhere.
 	var dB = document.body;
 
+	// Centralized logic to create a label and input with specified name & id
+	// Probably could refactor this to html.js
 	var makeInputAndLabel = function(name, id) {
 		var labelEle = htmlModule.appendAndCreateTextNode(htmlModule.createElement("label", ""), name);
 		labelEle.appendChild(htmlModule.createElement("input", id));
@@ -34,29 +37,41 @@ var labModule = function() {
 	};
 
 	// Constructor
+	// Creates and initilizes, returns view.
 
 	function main() {
-		htmlModule = new htmlUtil();
-		arrModule = new arrMethods();
-		msg = htmlModule.msg;
+		htmlModule = new htmlUtil(); // View creation / HTML logic
+		arrModule = new arrMethods(); // Array Method shortcuts
+		msg = htmlModule.msg; // Logging
 		msg.set("Starting Module");
+
+		// Start battle module
 		var battle = new battleModule();
+		// Add Enemy
 		battle.enemy = new player("Troll", playerTypeEnum.TROLL, 50);
+		// Add Players
+		// This should be driven by player input (get name, get type)
 		battle.players.push(new player("Human", playerTypeEnum.WARRIOR, 30));
 		battle.players.push(new player("Human", playerTypeEnum.MAGE, 25));
+		// Do 1 round of battle.
 		battle.battle();
+		battle.round++; // increment the round number.
+		// Do action round
+		battle.actionRound();
 	}
 
 	return new main();
 };
 
-
+// Battle Module Class - main module for the "battle"
 var battleModule = function() {
-	var players = [];
-	var enemy = null;
-	var round = 0;
-	var battleRound = function() {
-		// Main battle logic
+	// Properties
+	var players = []; // Array of players
+	var enemy = null; // Enemy, could also be an array of enemies!
+	var round = 0; // Round, mainly for stats.  Each round should be Action/Battle
+
+	// Methods
+	var battleRound = function() { // Main battle logic
 		for (var i = 0; i < this.players.length; i++) {
 			// Player attacks first.
 			var plyr = this.players[i];
@@ -73,7 +88,7 @@ var battleModule = function() {
 		msg.set(troll.toString());
 	};
 
-	var actionRound = function(){
+	var actionRound = function() {
 		// Ask the player for input on the next round of action.
 		// If the player has a special item, then they are allowed special actions.
 	}
@@ -82,6 +97,7 @@ var battleModule = function() {
 		return "Battle Round: " + this.round + " Troll is at:" + (this.enemy) ? this.enemy.hp : "Dead!";
 	};
 
+	// Constructor
 	var battleModule = function() {
 		msg.set("Battle Module Start");
 		this.round = round;
@@ -94,20 +110,29 @@ var battleModule = function() {
 	return new battleModule();
 };
 
+// Player Class
 var player = function(_name, _type, _hp) {
+	// Properties
 	this.name = _name;
 	this.type = _type;
 	this.hp = _hp;
 	this.items = [];
 
+	// Methods
 	this.isDead = function() {
 		return (this.hp <= 0);
 	};
 	this.baseAttackPower = function() {
+
+		// The great thing about assigning an enum (object) to type
+		// Is that we don't need to nest if/switch statements to create some quick logic
+		// this.type.baseAttack will automatically == whatever type we are and it's properties.
 		return this.type.baseAttack;
 	};
 	this.extraAttackPower = function() {
 		var modifier = 0;
+		// Here we use a helper method from util.js arrayMethods that will return
+		// and integer (index) of whatever we are looking for.
 		if (arrModule.Contains(this.items, itemEnum.SWORD) > 0) {
 			modifier += itemEnum.SWORD.value;
 		}
@@ -118,12 +143,16 @@ var player = function(_name, _type, _hp) {
 		// Return a value with a modifier if they have a sword.
 		return this.baseAttackPower() + this.extraAttackPower();
 	};
+	// Common method to return some important properties in an easily readable format (and values)
 	this.toString = function() {
-		return (this.name + "["+ this.type.title +"]: " + this.hp + " | " + this.attackPower());
+		return (this.name + "[" + this.type.title + "]: " + this.hp + " | " + this.attackPower());
 	};
 
+	// Log
+	msg.set("Creating new player: " + this.toString());
 };
 
+// Item Class
 var item = function(name, value) {
 	this.name = name;
 	this.value = value;
@@ -131,6 +160,7 @@ var item = function(name, value) {
 
 
 /// ENUMS
+// Enum with object of properties, self assigned
 var playerTypeEnum = {
 	WARRIOR: {
 		title: "Warrior",
@@ -146,6 +176,8 @@ var playerTypeEnum = {
 	}
 };
 
+// Enum with object of properties, from "base" class/method.
+// Usage itemEnum.BOOTSOFFLIGHT.name would return "Boots of flight"
 var itemEnum = {
 	BOOTSOFFLIGHT: new item("Boots of flight", 0),
 	DUSTOFFLIGHT: new item("Magic Dust", 0),
